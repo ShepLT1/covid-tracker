441c6ef7ec150ff45b852932118ff59cd6b314d7
ShepLT1 <lshepherd234@gmail.com>
2020-07-25 11:36:33 -0700

    Added js object for chart (currently commented out), created loop to retrieve data from past month, format each day for past month, push each date to x axis, # of daily positive tests to y axis

--- Develop/script-ls.js
+++ Develop/script-ls.js
@@ -65,6 +65,107 @@ $(document).ready(function() {
         // formats to user-friendly date
         var formatTodayDate = currentMonth + "/" + currentDay + "/" + currentYear;
 
+        // var ctx = $('#myChart').getContext('2d');
+
+        // var chart = new Chart(ctx, {
+    
+        //   // The type of chart we want to create
+        //   type: 'line',
+
+        //   // The data for our dataset
+        //   data: {
+        //     labels: [],
+        //     datasets: [{
+        //         label: 'COVID-19 Daily deaths',
+        //         backgroundColor: 'rgb(255, 99, 132)',
+        //         borderColor: 'rgb(255, 99, 132)',
+        //         data: []
+        //     }]
+        //   },
+
+        //   // Configuration options go here
+        //   options: {
+        //     scales: {
+        //       yAxes: [{
+        //         ticks: {
+        //             beginAtZero: true
+        //         }
+        //       }]
+        //     }
+        //   }
+        // });
+
+        for (i = 1; i < 32; i++) {
+
+          // cumulative death total
+          var totDeaths = response[i].death;
+          // new deaths for current day
+          var dailyDeaths = response[i].deathIncrease;
+
+          // cumulative hospitalized patient total
+          var totHospital = response[i].hospitalizedCumulative;
+          // current number of hospitalized patients
+          var currHospital = response[i].hospitalizedCurrently;
+          // number of new hospital patients admitted for this day
+          var incHospital = response[i].hospitalizedIncrease;
+
+          // cumulative ICU patient total
+          var totICU = response[i].inIcuCumulative;
+          // current number of ICU patients
+          var currICU = response[i].inIcuCurrently;
+
+          // cumulative total of negative tests
+          var totNegative = response[i].negative;
+
+          // cumulative total of patients put on ventilator
+          var totVent = response[i].onVentilatorCumulative;
+          // current number of patients on ventilator
+          var currVent = response[i].onVentilatorCurrently;
+
+          // cumulative total of positive tests
+          var totPositive = response[i].positive;
+          // number of new positive tests received on this day
+          var incPositive = response[i].positiveIncrease;
+
+          // cumulative total of number of tests administered
+          var totTests = response[i].totalTestResults;
+          // number of tests administered on this day
+          var incTests = response[i].totalTestResultsIncrease;
+
+          // pulls today's date from ajax call
+          var date = JSON.stringify(response[i].date);
+
+          console.log(totICU, totNegative, totPositive, totTests, totVent, totDeaths, totHospital, dailyDeaths, currHospital, currICU, currVent, incTests, incPositive, incHospital, date);
+
+          // grabs current month
+          var month = date.slice(4,6);
+
+          if (month.charAt(0) == 0) {
+
+            month = month.substring(1);
+
+          }
+
+          // grabs current day of month
+          var day = date.slice(6,8);
+
+          if (day.charAt(0) == 0) {
+
+            day = day.substring(1);
+
+          }
+
+          // formats to user-friendly date
+          var formatDate = month + "/" + day;
+
+          console.log(formatDate);
+
+          chart.data.labels.push(formatDate);
+
+          chart.data.datasets.data.push(incPositive);
+
+        }
+
         // find amount of ventilators or ICU beds available in each state, then compute current icu/availbe icu. ditto for ventilators
 
     })
