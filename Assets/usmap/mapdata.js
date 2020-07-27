ecdf0397ff7e844ccd3094972af8086a597c5fda
ShepLT1 <lshepherd234@gmail.com>
2020-07-26 18:55:06 -0700

    Changed mapdata.js code to open dashboard in same tab rather than new tab

--- Assets/usmap/mapdata.js
+++ Assets/usmap/mapdata.js
@@ -65,7 +65,7 @@ var simplemaps_usmap_mapdata={
 		div: "map",
     auto_load: "yes",
     rotate: "0",
-    url_new_tab: "yes",
+    url_new_tab: "no",
     images_directory: "default",
     import_labels: "no",
     fade_time: 0.1,
