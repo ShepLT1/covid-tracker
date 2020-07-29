276c198077737de1ad9ea9ccd963625b9fd983c0
George F <64555318+GAFelton@users.noreply.github.com>
2020-07-28 19:41:00 -0700

    Changed todayTotPositiveSpan from id to class

--- Develop/script-ls.js
+++ Develop/script-ls.js
@@ -90,12 +90,12 @@ $(document).ready(function() {
           }
 
           $("#updatedStatsDateSpan").text(NA(formatTodayDate));
-          $("#todayTotPositiveSpan").text(NA(todayTotPositive));
+          $(".todayTotPositiveSpan").text(NA(todayTotPositive));
           $("#todayIncPositiveSpan").text(NA(todayIncPositive));
           $("#todayTotDeathsSpan").text(NA(todayTotDeaths));
           $("#todayDailyDeathsSpan").text(NA(todayDailyDeaths));
 
-          $("#todayTotPositiveSpan").text(NA(todayTotPositive));
+          $(".todayTotPositiveSpan").text(NA(todayTotPositive));
           $("#todayTotNegativeSpan").text(NA(todayTotNegative));
           $("#todayTotTestsSpan").text(NA(todayTotTests));
           $("#todayIncTestsSpan").text(NA(todayIncTests));
