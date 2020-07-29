da61efe91cd9e12f106d72f0c7f45a1bf31d7a85
George F <64555318+GAFelton@users.noreply.github.com>
2020-07-28 21:35:14 -0700

    Added function setStateName(), pseudocode.

--- Develop/alertbar.js
+++ Develop/alertbar.js
@@ -2,8 +2,20 @@ var maskAdvisoryButton = $(".maskAdvisoryButton");
 var socialDistancingButton = $(".socialDistancingButton");
 var travelRestrictionsButton = $(".travelRestrictionsButton");
 var newCasesButton = $(".newCasesTrendButton");
+var stateName = $(".stateName");
 
+function setStateName() {
+    var stateAbbrev = localStorage.getItem("Abbreviation");
+    if (stateAbbrev.length > 2) {
+        stateAbbrev = stateAbbrev.substr(7, 2);
+    }
 
+    // Find data-abbrev that matches stateAbbrev.
+    // Get state title from same element.
+    // Display state title in stateName.
+}
+
+// External Search Buttons for Alerts Bar
 function runYourSearch(search) {
     var stateAbbrev = localStorage.getItem("Abbreviation");
     if (stateAbbrev.length > 2) {
