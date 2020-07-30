458fb9ca34626cfbffceab26e06ca91c975f7626
Julee Butler <jiny2060@hotmail.com>
2020-07-30 09:11:37 -0700

    update maptest.js link with index.html

--- Develop/maptest.js
+++ Develop/maptest.js
@@ -1,51 +1,3 @@
-//creating base map using google map 
-var map;
-    function initMap() {
-        const maparea = document.getElementById("map");
-        // console.log(maparea)
-        map = new google.maps.Map(maparea, {
-        zoom: 4,
-        center: {lat: 39.5, lng: -98.35},
-        mapTypeId: 'terrain'
-      });
-//calling api using AJAX 
-    var queryURL = "https://www.trackcorona.live/api/cities";
-    $.ajax({
-      type: "GET",
-      url: queryURL,
-      dataType: "json",
-      success: function (response) {
-        for (var i = 0; i < response.data.length; i++) {
-          var lat = response.data[i]?.latitude;
-          var long = response.data[i]?.longitude;
-          var latLng = new google.maps.LatLng(lat, long);
-          var radiuscon = response.data[i]?.confirmed / 2;
-//creating circle on the map
-    const cityCircle = new google.maps.Circle({
-      strokeColor: "#FF0000",
-      strokeOpacity: 0.8,
-      strokeWeight: 2,
-      fillColor: "#FF0000",
-      fillOpacity: 0.35,
-      map,
-      center: latLng,
-      radius: radiuscon,
-      title: response.data[i]?.location,
-      content: ["Confirmed Cases: " + response.data[i]?.confirmed,
-                "Death: " + response.data[i]?.dead]
-      }); 
-      var infoWindow= new google.maps.InfoWindow({content: ""});
-//add a click event to the circle
-google.maps.event.addListener(cityCircle, 'click', function(ev){
-  infoWindow.setContent('<h3>'+this.title+'</h3>' + "<div>"+this.content[0] +"<br>"+this.content[1]+"</div>");
-    infoWindow.setPosition(cityCircle.getCenter());
-    infoWindow.open(map);
-});
-    }
-  }
-  });
-}
-
 //creating array for state coordinates
 var states = [
   {state: "al",
@@ -203,25 +155,86 @@ var states = [
     longitude: -107.5512}
   ]
 
-  //function and eventlistener to reset center of map on selected state
+//initial map is created to display on dashboard
+var map;
+    function initMap() {
+        const maparea = document.getElementById("map");
+        map = new google.maps.Map(maparea, {
+        zoom: 4,
+        center: {lat: 39.5, lng: -98.35},
+        mapTypeId: 'terrain'
+      });
+
+//calling api using AJAX 
+    var queryURL = "https://www.trackcorona.live/api/cities";
+    $.ajax({
+      type: "GET",
+      url: queryURL,
+      dataType: "json",
+      success: function (response) {
+        for (var i = 0; i < response.data.length; i++) {
+          var lat = response.data[i]?.latitude;
+          var long = response.data[i]?.longitude;
+          var latLng = new google.maps.LatLng(lat, long);
+          var radiuscon = response.data[i]?.confirmed / 2;
+
+//creating circle on the map with infowindow
+const cityCircle = new google.maps.Circle({
+  strokeColor: "#FF0000",
+  strokeOpacity: 0.8,
+  strokeWeight: 2,
+  fillColor: "#FF0000",
+  fillOpacity: 0.35,
+  map,
+  center: latLng,
+  radius: radiuscon,
+  title: response.data[i]?.location,
+  content: ["Confirmed Cases: " + response.data[i]?.confirmed,
+            "Death: " + response.data[i]?.dead]
+    }); 
+  var infoWindow= new google.maps.InfoWindow({content: ""});
+//add a click event to the circle
+google.maps.event.addListener(cityCircle, 'click', function(ev){
+  infoWindow.setContent('<h3>'+this.title+'</h3>' + "<div>"+this.content[0] +"<br>"+this.content[1]+"</div>");
+  infoWindow.setPosition(cityCircle.getCenter());
+  infoWindow.open(map);
+  });
+}
+}
+});
+
+//for index page eventlistener to click state
+var abbrev = localStorage.getItem("Abbreviation");
+abbrev = abbrev.substring(7)
+
+  for (var i = 0; i < states.length; i++) {
+    var lat = states[i].latitude;
+    var lng = states[i].longitude;
+    if (abbrev === states[i].state) {
+    map.setZoom(6);
+    map.setCenter(new google.maps.LatLng(lat, lng));
+    }
+  
+};
+}
+
+//function and eventlistener to reset center of map on selected state
 $(".selector-dropdown").on("click",function(event) {
-
   var stateAbr = $(this).attr("data-state-abbr");
-
   localStorage.setItem("Abbreviation", "states/" + stateAbr);
 
   for (var i = 0; i < states.length; i++) {
     var lat = states[i].latitude;
     var lng = states[i].longitude;
-
     if (stateAbr === states[i].state) {
     map.setZoom(6);
     map.setCenter(new google.maps.LatLng(lat, lng));
     }
-  //eventlisterner for country button to display whole us 
-  $(".selector-dropdown-country").on("click", function(event) {
-    map.setZoom(4);
-    map.setCenter(new google.maps.LatLng(39.5, -98.35)); 
+  
+//eventlisterner for country button to display whole us 
+$(".selector-dropdown-country").on("click", function(event) {
+  map.setZoom(4);
+  map.setCenter(new google.maps.LatLng(39.5, -98.35)); 
   })
-  }
+}
 });
