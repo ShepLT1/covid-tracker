1a66d3d3cf9cb80de6c3449d6f9bc5e6048fb624
George F <64555318+GAFelton@users.noreply.github.com>
2020-07-30 15:41:42 -0700

    Screenreader links now work as intended.

--- Assets/usmap/mapscript.js
+++ Assets/usmap/mapscript.js
@@ -1,20 +1,18 @@
+$("document").ready(function(){
+  
 simplemaps_usmap.hooks.click_state = function (id) {
   localStorage.setItem("Abbreviation", "states/" + simplemaps_usmap_mapdata.state_specific[id].abbrev);
 }
 
-$(".sr-state-selector").on("click", function() {
+$(".sr-state-selector").click( function() {
   var stateAbbrevForStorage = $(this).data("state-abbrev");
   console.log(stateAbbrevForStorage);
-  if (stateAbbrevForStorage = "us") {
+  if (stateAbbrevForStorage === "us") {
     localStorage.setItem("Abbreviation", stateAbbrevForStorage);
   }
   else {
     localStorage.setItem("Abbreviation", "states/" + stateAbbrevForStorage);
   }
-})
-
-$(".sr-state-selector").on('keydown', (event) => {
-  if (event.code === 'Enter') {
-    $(".sr-state-selector").click();
-  }
 });
+
+})
