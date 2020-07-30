e46c29305ff8a3f5eac0df0974445c359fdda48a
George F <64555318+GAFelton@users.noreply.github.com>
2020-07-30 14:59:39 -0700

    Screenreader link testing.

--- Assets/usmap/mapscript.js
+++ Assets/usmap/mapscript.js
@@ -1,3 +1,20 @@
-simplemaps_usmap.hooks.click_state = function(id){
-    localStorage.setItem("Abbreviation", "states/" + simplemaps_usmap_mapdata.state_specific[id].abbrev);
+simplemaps_usmap.hooks.click_state = function (id) {
+  localStorage.setItem("Abbreviation", "states/" + simplemaps_usmap_mapdata.state_specific[id].abbrev);
+}
+
+$(".sr-state-selector").on("click", function() {
+  var stateAbbrevForStorage = $(this).data("state-abbrev");
+  console.log(stateAbbrevForStorage);
+  if (stateAbbrevForStorage = "us") {
+    localStorage.setItem("Abbreviation", stateAbbrevForStorage);
   }
+  else {
+    localStorage.setItem("Abbreviation", "states/" + stateAbbrevForStorage);
+  }
+})
+
+$(".sr-state-selector").on('keydown', (event) => {
+  if (event.code === 'Enter') {
+    $(".sr-state-selector").click();
+  }
+});
