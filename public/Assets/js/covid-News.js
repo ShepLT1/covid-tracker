5b7c0cb9a7423cf50c6f82a0e1f70ef79ec694d9
George F <64555318+GAFelton@users.noreply.github.com>
2020-12-17 11:45:52 -0800

    Set up firebase app, moved base project files into /public directory.

--- public/Assets/js/covid-News.js
+++ public/Assets/js/covid-News.js
@@ -6,17 +6,8 @@ var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=covid
         method: "GET"
       }).then(function(response) {
         for (var i = 0; i < 3; i++) {
-    
   $("#headline" + i).text(response.response.docs[i].headline.main);
-
-      console.log(response.response.docs[i].headline.main);
-
- $("#description" + i).text(response.response.docs[i].abstract);
-      
-      console.log(response.response.docs[i].abstract);
-
+  $("#description" + i).text(response.response.docs[i].abstract);
   $("#url" + i).attr("href",response.response.docs[i].web_url).text("Article Source");
-
-      console.log(response.response.docs[i].web_url);
   }
 });
